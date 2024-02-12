cd7b6e54791ad2b8ad9c6064a5ce0e1d33282fdd
Zac <zacolden@gmail.com>
2024-02-13 02:03:46 +1100

    Fix colour formatting for Windows

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.botbuddy.net/botbuddy/goagent
 
 go 1.22.0
+
+require golang.org/x/sys v0.17.0 // indirect
---
+++ main_windows.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"golang.org/x/sys/windows"
+	"os"
+)
+
+func init() {
+	var outMode uint32
+	out := windows.Handle(os.Stdout.Fd())
+	if err := windows.GetConsoleMode(out, &outMode); err != nil {
+		return
+	}
+	outMode |= windows.ENABLE_PROCESSED_OUTPUT | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+	_ = windows.SetConsoleMode(out, outMode)
+}
